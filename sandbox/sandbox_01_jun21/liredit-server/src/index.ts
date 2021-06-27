1b2e03e0330e51db1332079ae34bef76702bcbf7
JonasMartins <jonas.w.martins@gmail.com>
2021-06-27 01:05:53 -0300

    On branch sprint_01_jun21  Changes to be committed: 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/constants.js 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/constants.js.map 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/entities/Post.js 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/entities/Post.js.map 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/index.js 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/index.js.map 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/migrations/Migration20210626212159.js 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/migrations/Migration20210626212159.js.map 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/mikro-orm.config.js 	new file:   sandbox/sandbox_01_jun21/liredit-server/dist/mikro-orm.config.js.map 	modified:   sandbox/sandbox_01_jun21/liredit-server/package.json 	new file:   sandbox/sandbox_01_jun21/liredit-server/src/constants.ts 	new file:   sandbox/sandbox_01_jun21/liredit-server/src/entities/Post.ts 	modified:   sandbox/sandbox_01_jun21/liredit-server/src/index.ts 	new file:   sandbox/sandbox_01_jun21/liredit-server/src/migrations/Migration20210626212159.ts 	new file:   sandbox/sandbox_01_jun21/liredit-server/src/mikro-orm.config.ts
        - Starting setup with mikroORM.
        - Set typescript with node to build the server.
        - Tutorial video stopped at 38min.
    
        - MikroORM cannot persist data at postgres on WSL.
        - Evaluate the possibility of installing postgres on windows.

--- sandbox/sandbox_01_jun21/liredit-server/src/index.ts
+++ sandbox/sandbox_01_jun21/liredit-server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('test')
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig); // connect to database
+    orm.getMigrator().up(); // run migrations
+    const post = orm.em.create(Post, { title: "First Post" }); // run a sql
+    await orm.em.persistAndFlush(post); // persist on table
+};
+
+main().catch((err) => {
+    console.error(err);
+});
