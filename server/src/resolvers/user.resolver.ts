3dc06490c427645fdd86e29cbe81ffe4f6a9bfb6
jonasMartins <jonas.w.martins@gmail.com>
2021-09-08 23:58:50 -0300

    Changes to be committed: 	modified:   server/package.json 	modified:   server/src/application.ts 	modified:   server/src/resolvers/user.resolver.ts 	modified:   server/src/utils/sendRefreshToken.ts
    	- Finnaly solving running project on macOs.
    	- .env file typing error.

--- server/src/resolvers/user.resolver.ts
+++ server/src/resolvers/user.resolver.ts
@@ -1,14 +1,14 @@
 import {
-    Arg,
-    Ctx,
-    Field,
-    InputType,
-    Mutation,
-    ObjectType,
-    Query,
-    Resolver,
-    UseMiddleware,
-    Int,
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  UseMiddleware,
+  Int,
 } from "type-graphql";
 import { User } from "../entities/user.entity";
 import argon2 from "argon2";
@@ -21,248 +21,247 @@ import { COOKIE_NAME } from "../constants";
 import { Role } from "./../entities/role.entity";
 @InputType()
 class UserBasicData {
-    @Field()
-    name: string;
-    @Field()
-    password: string;
-    @Field()
-    email: string;
-    @Field()
-    role_id: string;
+  @Field()
+  name: string;
+  @Field()
+  password: string;
+  @Field()
+  email: string;
+  @Field()
+  role_id: string;
 }
 
 @ObjectType()
 class tokenAndId {
-    @Field(() => String, { nullable: true })
-    accessToken?: string;
+  @Field(() => String, { nullable: true })
+  accessToken?: string;
 
-    @Field(() => String, { nullable: true })
-    userId?: string;
+  @Field(() => String, { nullable: true })
+  userId?: string;
 }
 
 @ObjectType()
 class LoginResponse {
-    @Field(() => [ErrorFieldHandler], { nullable: true })
-    errors?: ErrorFieldHandler[];
-    @Field(() => tokenAndId, { nullable: true })
-    result?: tokenAndId;
+  @Field(() => [ErrorFieldHandler], { nullable: true })
+  errors?: ErrorFieldHandler[];
+  @Field(() => tokenAndId, { nullable: true })
+  result?: tokenAndId;
 }
 
 @ObjectType()
 class UserResponse {
-    @Field(() => [ErrorFieldHandler], { nullable: true })
-    errors?: ErrorFieldHandler[];
-    @Field(() => User, { nullable: true })
-    user?: User;
+  @Field(() => [ErrorFieldHandler], { nullable: true })
+  errors?: ErrorFieldHandler[];
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @ObjectType()
 class RevokeResponse {
-    @Field(() => Boolean)
-    incrementado: true | false;
-    @Field(() => Int, { nullable: true })
-    version: number;
+  @Field(() => Boolean)
+  incrementado: true | false;
+  @Field(() => Int, { nullable: true })
+  version: number;
 }
 
 @Resolver()
 export class UserResolver {
-    @Query(() => String)
-    hello() {
-        return "Hello";
-    }
-    // altera a versão do token inpossibilitando
-    // novo acesso se a versão for diferente
-    // usado quando o usuário esqueceu a senha
-    @Mutation(() => RevokeResponse)
-    async revokeRefreshTokensForUser(
-        @Arg("userId") userId: string,
-        @Ctx() { em }: Context
-    ): Promise<RevokeResponse> {
-        const user = await em.findOne(User, { id: userId });
-        const result: RevokeResponse = {
-            version: 0,
-            incrementado: false,
-        };
-        try {
-            if (user) {
-                user.tokenVersion = user.tokenVersion + 1;
-                result.version = user.tokenVersion;
-                result.incrementado = true;
-                await em.persistAndFlush(user);
-            } else {
-                result.version = 0;
-                result.incrementado = false;
-            }
-        } catch (_) {
-            result.version = 0;
-            result.incrementado = false;
-        }
-
-        return result;
+  @Query(() => String)
+  hello() {
+    return "Hello";
+  }
+  // altera a versão do token inpossibilitando
+  // novo acesso se a versão for diferente
+  // usado quando o usuário esqueceu a senha
+  @Mutation(() => RevokeResponse)
+  async revokeRefreshTokensForUser(
+    @Arg("userId") userId: string,
+    @Ctx() { em }: Context
+  ): Promise<RevokeResponse> {
+    const user = await em.findOne(User, { id: userId });
+    const result: RevokeResponse = {
+      version: 0,
+      incrementado: false,
+    };
+    try {
+      if (user) {
+        user.tokenVersion = user.tokenVersion + 1;
+        result.version = user.tokenVersion;
+        result.incrementado = true;
+        await em.persistAndFlush(user);
+      } else {
+        result.version = 0;
+        result.incrementado = false;
+      }
+    } catch (_) {
+      result.version = 0;
+      result.incrementado = false;
     }
 
-    @Query(() => String)
-    @UseMiddleware(isAuth)
-    logedInTest(@Ctx() { payload }: Context) {
-        return `Hi, yout id is : ${payload!.userId}`;
+    return result;
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  logedInTest(@Ctx() { payload }: Context) {
+    return `Hi, yout id is : ${payload!.userId}`;
+  }
+
+  @Query(() => UserResponse)
+  async getUserById(
+    @Arg("id") id: string,
+    @Ctx() { em }: Context
+  ): Promise<UserResponse> {
+    const user = await em.getRepository(User).findOne({ id });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "id",
+            message: "User not Fount",
+            method: `Method: login, at ${__filename}`,
+          },
+        ],
+      };
     }
 
-    @Query(() => UserResponse)
-    async getUserById(
-        @Arg("id") id: string,
-        @Ctx() { em }: Context
-    ): Promise<UserResponse> {
-        const user = await em.getRepository(User).findOne({ id });
-        if (!user) {
-            return {
-                errors: [
-                    {
-                        field: "id",
-                        message: "User not Fount",
-                        method: `Method: login, at ${__filename}`,
-                    },
-                ],
-            };
-        }
+    return { user };
+  }
 
-        return { user };
+  @Mutation(() => UserResponse)
+  async createUser(
+    @Arg("options") options: UserBasicData,
+    @Ctx() { em }: Context
+  ): Promise<UserResponse> {
+    if (options.name.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "A username must have length greater than 2 charachters.",
+            method: `Method: createUser, at ${__filename}`,
+          },
+        ],
+      };
     }
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message:
+              "A user password must have length greater than 3 charachters.",
+            method: `Method: createUser, at ${__filename}`,
+          },
+        ],
+      };
+    }
+    const hashedPassword = await argon2.hash(options.password);
+    options.password = hashedPassword;
 
-    @Mutation(() => UserResponse)
-    async createUser(
-        @Arg("options") options: UserBasicData,
-        @Ctx() { em }: Context
-    ): Promise<UserResponse> {
-        if (options.name.length <= 2) {
-            return {
-                errors: [
-                    {
-                        field: "username",
-                        message:
-                            "A username must have length greater than 2 charachters.",
-                        method: `Method: createUser, at ${__filename}`,
-                    },
-                ],
-            };
-        }
-        if (options.password.length <= 3) {
-            return {
-                errors: [
-                    {
-                        field: "password",
-                        message:
-                            "A user password must have length greater than 3 charachters.",
-                        method: `Method: createUser, at ${__filename}`,
-                    },
-                ],
-            };
-        }
-        const hashedPassword = await argon2.hash(options.password);
-        options.password = hashedPassword;
+    const role = await em.findOne(Role, {
+      id: options.role_id,
+    });
 
-        const role = await em.findOne(Role, {
-            id: options.role_id,
-        });
-
-        if (!role) {
-            return {
-                errors: [
-                    {
-                        field: "role_id",
-                        message: "Could not find role, from role_id provided.",
-                        method: `Method: createUser, at ${__filename}`,
-                    },
-                ],
-            };
-        }
-        /*
+    if (!role) {
+      return {
+        errors: [
+          {
+            field: "role_id",
+            message: "Could not find role, from role_id provided.",
+            method: `Method: createUser, at ${__filename}`,
+          },
+        ],
+      };
+    }
+    /*
             name: options.name,
             hashedPassword: options.password,
             role: role,
             email: options.email
         */
-        const user = em.create(User, {
-            name: options.name,
-            password: options.password,
-            role_id: options.role_id,
-            email: options.email,
+    const user = em.create(User, {
+      name: options.name,
+      password: options.password,
+      role_id: options.role_id,
+      email: options.email,
+    });
+
+    user.role = role;
+
+    await em.persistAndFlush(user);
+
+    return { user };
+  }
+
+  @Mutation(() => Boolean!, { nullable: true })
+  async logout(@Ctx() { res }: Context): Promise<void> {
+    if (typeof window !== "undefined") {
+      localStorage.clear();
+    }
+    res.clearCookie(COOKIE_NAME);
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email", () => String!) email: string,
+    @Arg("password", () => String!) password: string,
+    @Ctx() { em, res }: Context
+  ): Promise<LoginResponse> {
+    if (!email || !password) {
+      let errors: ErrorFieldHandler[] | undefined = [];
+
+      if (!email) {
+        errors = [
+          {
+            field: "email",
+            message: "Email is required",
+            method: `Method: login, at ${__filename}`,
+          },
+        ];
+      }
+
+      if (!password) {
+        errors.push({
+          field: "password",
+          message: "Password is required",
+          method: `Method: login, at ${__filename}`,
         });
+      }
 
-        user.role = role;
-
-        await em.persistAndFlush(user);
-
-        return { user };
+      return {
+        errors,
+      };
     }
 
-    @Mutation(() => Boolean!, { nullable: true })
-    async logout(@Ctx() { res }: Context): Promise<void> {
-        if (typeof window !== "undefined") {
-            localStorage.clear();
-        }
-        res.clearCookie(COOKIE_NAME);
+    const user = await em.findOne(User, { email: email });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "email",
+            message: "Email not found",
+            method: `Method: login, at ${__filename}`,
+          },
+        ],
+      };
     }
 
-    @Mutation(() => LoginResponse)
-    async login(
-        @Arg("email", () => String!) email: string,
-        @Arg("password", () => String!) password: string,
-        @Ctx() { em, res }: Context
-    ): Promise<LoginResponse> {
-        if (!email || !password) {
-            let errors: ErrorFieldHandler[] | undefined = [];
+    const validPass = await argon2.verify(user.password, password);
 
-            if (!email) {
-                errors = [
-                    {
-                        field: "email",
-                        message: "Email is required",
-                        method: `Method: login, at ${__filename}`,
-                    },
-                ];
-            }
-
-            if (!password) {
-                errors.push({
-                    field: "password",
-                    message: "Password is required",
-                    method: `Method: login, at ${__filename}`,
-                });
-            }
-
-            return {
-                errors,
-            };
-        }
-
-        const user = await em.findOne(User, { email: email });
-
-        if (!user) {
-            return {
-                errors: [
-                    {
-                        field: "email",
-                        message: "Email not found",
-                        method: `Method: login, at ${__filename}`,
-                    },
-                ],
-            };
-        }
-
-        const validPass = await argon2.verify(user.password, password);
-
-        if (!validPass) {
-            return {
-                errors: [
-                    {
-                        field: "password",
-                        message: "Incorrect password",
-                        method: `Method: login, at ${__filename}`,
-                    },
-                ],
-            };
-        }
-        /*
+    if (!validPass) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Incorrect password",
+            method: `Method: login, at ${__filename}`,
+          },
+        ],
+      };
+    }
+    /*
         if (typeof window === "undefined") {
             /**
              *
@@ -280,18 +279,18 @@ export class UserResolver {
         } 
         */
 
-        if (typeof window !== "undefined") {
-            localStorage.setItem("currentUserId", `${user.id}`);
-        }
-        sendRefreshToken(res, createRefreshToken(user));
-
-        const result: tokenAndId = {
-            accessToken: createAcessToken(user),
-            userId: user.id,
-        };
-
-        return {
-            result,
-        };
+    if (typeof window !== "undefined") {
+      localStorage.setItem("currentUserId", `${user.id}`);
     }
+    sendRefreshToken(res, createRefreshToken(user));
+
+    const result: tokenAndId = {
+      accessToken: createAcessToken(user),
+      userId: user.id,
+    };
+
+    return {
+      result,
+    };
+  }
 }
