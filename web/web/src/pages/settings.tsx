db5916c0a3d4a94ac15c816841e55e048a620a7b
JonasMartins <jonas.w.martins@gmail.com>
2021-10-03 18:40:28 -0300

    Changes to be committed: 	modified:   web/web/src/pages/settings.tsx
        - Ajusting layout of settings page

--- web/web/src/pages/settings.tsx
+++ web/web/src/pages/settings.tsx
@@ -11,6 +11,8 @@ import {
     Input,
     Flex,
     Image,
+    Stack,
+    Select,
     FormErrorMessage,
 } from "@chakra-ui/react";
 import { Form, Formik, Field } from "formik";
@@ -63,21 +65,40 @@ const Settings: React.FC<settingsProps> = ({}) => {
     }, [fetching]);
 
     // if (error) return <p>Oh no... {error.message}</p>;
-
+    // /* topo | direita | inferior | esquerda */
     const content = (
         <Container>
             <Navbar />
-            <Flex flexDir="column" alignItems="center">
+
+            <Flex
+                flexDir="row"
+                p={3}
+                m={[3, 3, 10, 3]}
+                justifyContent="flex-start"
+            >
                 {data && data?.getUserSettings?.user?.picure ? (
-                    <Flex mt={5}>
+                    <Flex
+                        mt={5}
+                        flexDir="column"
+                        alignItems="center"
+                        justifyContent="flex-start"
+                        flexGrow={1}
+                    >
                         <Image
                             boxSize="100px"
                             borderRadius="full"
                             src={data?.getUserSettings?.user?.picure}
                         />
+                        <Text>{data?.getUserSettings?.user?.role?.name}</Text>
                     </Flex>
                 ) : (
-                    <Flex mt={5}>
+                    <Flex
+                        mt={5}
+                        flexDir="column"
+                        alignItems="center"
+                        justifyContent="flex-start"
+                        flexGrow={0.3}
+                    >
                         <Avatar
                             size="100px"
                             round={true}
@@ -85,73 +106,115 @@ const Settings: React.FC<settingsProps> = ({}) => {
                                 data?.getUserSettings?.user?.name || "Foo bar"
                             }
                         />
+                        <Text>{data?.getUserSettings?.user?.role?.name}</Text>
                     </Flex>
                 )}
+                <Flex flexDir="column" alignItems="stretch" flexGrow={0.4}>
+                    <Formik
+                        initialValues={{
+                            name: userInfo.user.name,
+                            email: userInfo.user.email,
+                            role: userInfo.user.role,
+                        }}
+                        onSubmit={() => {
+                            console.log("submited");
+                        }}
+                    >
+                        {(props) => (
+                            <Form {...props}>
+                                <Stack spacing={3}>
+                                    <Field name="name">
+                                        {({ field, form }) => (
+                                            <FormControl
+                                                isInvalid={form.errors.name}
+                                            >
+                                                <FormLabel htmlFor="name">
+                                                    Name
+                                                </FormLabel>
+                                                <Input
+                                                    {...field}
+                                                    id="name"
+                                                    borderRadius="2em"
+                                                    size="lg"
+                                                    value={userInfo.user.name}
+                                                />
+                                                <FormErrorMessage>
+                                                    {form.errors.name}
+                                                </FormErrorMessage>
+                                            </FormControl>
+                                        )}
+                                    </Field>
+                                    <Field name="email">
+                                        {({ field, form }) => (
+                                            <FormControl
+                                                isInvalid={form.errors.email}
+                                            >
+                                                <FormLabel htmlFor="email">
+                                                    Email
+                                                </FormLabel>
+                                                <Input
+                                                    {...field}
+                                                    id="email"
+                                                    borderRadius="2em"
+                                                    size="lg"
+                                                    value={userInfo.user.email}
+                                                />
+                                                <FormErrorMessage>
+                                                    {form.errors.email}
+                                                </FormErrorMessage>
+                                            </FormControl>
+                                        )}
+                                    </Field>
 
-                <Text>{data?.getUserSettings?.user?.role?.name}</Text>
-                <Formik
-                    initialValues={{
-                        name: userInfo.user.name,
-                        email: userInfo.user.email,
-                        role: userInfo.user.role,
-                    }}
-                    onSubmit={() => {
-                        console.log("submited");
-                    }}
-                >
-                    {(props) => (
-                        <Form>
-                            <Field name="name">
-                                {({ field, form }) => (
-                                    <FormControl isInvalid={form.errors.name}>
-                                        <FormLabel htmlFor="name">
-                                            Name
-                                        </FormLabel>
-                                        <Input
-                                            {...field}
-                                            id="name"
-                                            borderRadius="2em"
-                                            size="lg"
-                                            // value={userInfo.name}
-                                        />
-                                        <FormErrorMessage>
-                                            {form.errors.name}
-                                        </FormErrorMessage>
-                                    </FormControl>
-                                )}
-                            </Field>
-                            <Field name="email">
-                                {({ field, form }) => (
-                                    <FormControl isInvalid={form.errors.email}>
-                                        <FormLabel htmlFor="email">
-                                            Email
-                                        </FormLabel>
-                                        <Input
-                                            {...field}
-                                            id="email"
-                                            borderRadius="2em"
-                                            size="lg"
-                                            // value={userInfo.email}
-                                        />
-                                        <FormErrorMessage>
-                                            {form.errors.email}
-                                        </FormErrorMessage>
-                                    </FormControl>
-                                )}
-                            </Field>
-                            <Button
-                                isLoading={props.isSubmitting}
-                                type="submit"
-                                variant="cyan-gradient"
-                                borderRadius="2em"
-                                size="md"
-                                flexFlow="row"
-                            >
-                                Save
-                            </Button>
-                        </Form>
-                    )}
-                </Formik>
+                                    <Field name="role">
+                                        {({ field, form }) => (
+                                            <FormControl
+                                                isInvalid={form.errors.role}
+                                            >
+                                                <FormLabel htmlFor="role">
+                                                    Role
+                                                </FormLabel>
+
+                                                <Select
+                                                    placeholder="Role"
+                                                    {...field}
+                                                    id="role"
+                                                    borderRadius="2em"
+                                                    size="lg"
+                                                >
+                                                    <option value="option1">
+                                                        Option 1
+                                                    </option>
+                                                    <option value="option2">
+                                                        Option 2
+                                                    </option>
+                                                    <option value="option3">
+                                                        Option 3
+                                                    </option>
+                                                </Select>
+
+                                                <FormErrorMessage>
+                                                    {form.errors.role}
+                                                </FormErrorMessage>
+                                            </FormControl>
+                                        )}
+                                    </Field>
+                                    <Button
+                                        isLoading={props.isSubmitting}
+                                        type="submit"
+                                        variant="cyan-gradient"
+                                        borderRadius="2em"
+                                        size="md"
+                                        flexFlow="row"
+                                        mt={4}
+                                    >
+                                        Save
+                                    </Button>
+                                </Stack>
+                            </Form>
+                        )}
+                    </Formik>
+                </Flex>
             </Flex>
             <Footer />
         </Container>
